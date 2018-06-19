a8b065fc890f84f8081bf579ed75dcde2814c480
Vibhor Gulati <vibhorgulati7@gmail.com>
2018-06-19 10:18:44 +0530

    Second Commit

--- CopyData.java
+++ CopyData.java
@@ -3,10 +3,10 @@ package copyingData;
 import java.io.*;
 
 public class CopyData {
-
+//W:\Super30\New folder\src\copyingData
 	public static void transferData() throws IOException {
-		File f1 = new File("W:/Super30/New folder/src/abc.txt");
-		File f2 = new File("W:/Super30/New folder/src/xyz.txt");
+		File f1 = new File("W:/Super30/New folder/src/copyingData/abc.txt");
+		File f2 = new File("W:/Super30/New folder/src/copyingData/xyz.txt");
 		f2.createNewFile();
 		FileInputStream fin = new FileInputStream(f1);
 		FileOutputStream fout = new FileOutputStream(f2);
@@ -26,7 +26,7 @@ public class CopyData {
 	}
 
 	public static void printData() throws IOException {
-		File f = new File("W:/Super30/New folder/src/xyz.txt");
+		File f = new File("W:/Super30/New folder/src/copyingData/xyz.txt");
 		FileReader fr = new FileReader(f);
 		int ch = fr.read();
 		while (ch != -1) {
